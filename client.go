abdfd85f5aed4d2d9ff7f67c3ba78bab7c9975e9
Cheryl <769303522@qq.com>
2023-05-14 16:21:23 +0800

    invoke method

--- client.go
+++ client.go
@@ -1,22 +1,32 @@
-package vermouth
+package client
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
+	jsoniter "github.com/json-iterator/go"
 	"google.golang.org/grpc"
 )
 
 const (
 	Success = "success"
+	GetMethod = "GET"
+	PostMethod = "POST"
 )
 
 type RpcClient struct {
-	addr    string
-	conn    *grpc.ClientConn
-	service VermouthGrpcClient
+	addr       string
+	conn       *grpc.ClientConn
+	service    VermouthGrpcClient
+	httpClient *http.Client
 
 	port      int64
 	prefix    string
@@ -26,6 +36,21 @@ type RpcClient struct {
 func NewVermouthRpcClient(remoteAddr string) *RpcClient {
 	client := &RpcClient{
 		addr: remoteAddr,
+		httpClient: &http.Client{
+			Timeout: 10 * time.Second,
+			Transport: &http.Transport{
+				Dial: (&net.Dialer{
+					Timeout:   10 * time.Second, //限制建立TCP连接的时间
+					KeepAlive: 10 * time.Second,
+				}).Dial,
+				ForceAttemptHTTP2:     true,
+				MaxIdleConnsPerHost:   10,
+				MaxConnsPerHost:       10,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+			},
+		},
 	}
 	con, err := grpc.Dial(remoteAddr, grpc.WithInsecure())
 	if err != nil {
@@ -100,6 +125,69 @@ func (client *RpcClient) JoinCluster(tcpAddress, name string) error {
 		return errors.New("server has encounter some error, please check server log")
 	}
 	return nil
+}
+
+func (client *RpcClient) loadBalance(port int64, prefix, fact string) (string, error) {
+	req := &LoadBalanceReq{
+		Port:   port,
+		Prefix: prefix,
+		Fact:   fact,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	res, err := client.service.LoadBalance(ctx, req)
+	if err != nil {
+		return "", err
+	}
+	if res.Message == "" {
+		return "", errors.New("can't find any host")
+	}
+	return res.GetMessage(), nil
+}
+
+func (client *RpcClient) sendHttpRequest(method, url string, data []byte) ([]byte, error) {
+	buf := bytes.NewBuffer(data)
+	req, err := http.NewRequest(method, url, buf)
+	if err != nil {
+		return nil, err
+	}
+	res, err := client.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	
+	body := res.Body
+	defer body.Close()
+	data, err = io.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (client *RpcClient) InvokeMethod(port int64, prefix, url, method string, body interface{}) []byte {
+	host, err := client.loadBalance(port, prefix, client.localAddr)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	reqUrl := fmt.Sprintf("http://%s%s", host, url)
+	var buf bytes.Buffer
+	if err := jsoniter.NewEncoder(&buf).Encode(body); err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	res, err := client.sendHttpRequest(method, reqUrl, buf.Bytes())
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	return res
+}
+
+func (client *RpcClient) SetHttpClient(hc *http.Client) {
+	client.httpClient = hc
 }
 
 func (client *RpcClient) Close() {
--- go.mod
+++ go.mod
@@ -2,8 +2,12 @@ module github.com/qiancijun/vermouth-go-client
 
 go 1.19
 
+require github.com/json-iterator/go v1.1.12
+
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
